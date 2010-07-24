8db5833d0babbd76d0ec3c64eb5b084ed49fc5df
Reese Hodge <quattro004@gmail.com>
2010-07-24 09:13:19 -0700

    Cut logo

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100619152818) do
+ActiveRecord::Schema.define(:version => 20100718163855) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(:version => 20100619152818) do
     t.string   "keywords",     :limit => 260
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.time     "prep_time"
+    t.time     "cook_time"
   end
 
   add_index "recipes", ["category_id"], :name => "fk_recipes_categories"
