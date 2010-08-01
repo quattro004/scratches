770d1d0dab3af3dacee085b3f4d3ffda9bab24b7
Reese Hodge <quattro004@gmail.com>
2010-08-01 10:04:11 -0700

    Broke out ingredients and got adding and removing to work. Started adding validations.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,6 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+
+  validates_presence_of :amount
+  validates_presence_of :name
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,12 @@
 class Recipe < ActiveRecord::Base
-  has_many :picture
+  has_many :picture, :dependent => :destroy
+  has_many :ingredient, :dependent => :destroy
   has_one :category
-  has_many :ingredient
+
+  accepts_nested_attributes_for :ingredient, :allow_destroy => true #, :reject_if => lambda { |a| a[:name].blank? || a[:amount].blank? }
+
+  validates_presence_of :title
+  validates_presence_of :instructions
+  validates_numericality_of :prep_time_in_minutes, :greater_than_or_equal_to => 1
+  validates_numericality_of :cook_time_in_minutes, :greater_than_or_equal_to => 1
 end
