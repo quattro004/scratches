b011149b9e5612b064aa34f89ef4b8bf5ad2d2ac
Reese Hodge <quattro004@gmail.com>
2010-09-04 09:15:34 -0700

    Fixed bug with adding ingredients dynamically. Still need to look into UJS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
       render(association.to_s.singularize + "_fields", :f => builder)
     end
     # TODO: find a replacement this doesn't exist in Rails 3
-    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
   end
 
 end
