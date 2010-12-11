608289b25c2aa2d56811f58ffa8d79a6a7ac947b
Reese Hodge <quattro004@gmail.com>
2010-12-11 09:33:56 -0800

    Limit the number of pictures a recipe can have to 6.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,4 +9,13 @@ class Recipe < ActiveRecord::Base
   validates_presence_of :instructions
   validates_numericality_of :prep_time_in_minutes, :greater_than_or_equal_to => 1
   validates_numericality_of :cook_time_in_minutes, :greater_than_or_equal_to => 1
+  validate :number_of_pictures?
+
+  private
+
+    def number_of_pictures?
+      unless picture.size < 6
+        errors.add :recipe, 'can only have a maximum of 6 pictures'
+      end
+    end
 end
