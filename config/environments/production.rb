f3992b2990c3ab5cfd62276a8c5d5f3aac84f745
quattro004 <quattro004@gmail.com>
2012-06-30 10:20:06 -0700

    Added some more action_mailer configuration for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,11 @@ Recipieces::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'recipieces.com' }
-  config.action_mailer.smtp_settings = { :enable_starttls_auto => false }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+# TODO: determine is this is necessary
+#  config.action_mailer.smtp_settings = { :enable_starttls_auto => false }
 
   # Compress JavaScript and CSS
   config.assets.compress = true
