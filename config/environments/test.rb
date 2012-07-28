66c91613f33dbc28674b7c3c13636353aa42bcc6
quattro004 <quattro004@gmail.com>
2012-07-28 12:30:49 -0700

    Adding cucumber tests for Devise.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,6 @@ Recipieces::Application.configure do
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
+
+  config.active_support.deprecation = :stderr
 end
