0513701fa4cb1d5a65b7221d778bed05643d100e
quattro004 <quattro004@gmail.com>
2012-08-19 12:21:11 -0700

    Redirect to login page on CanCan access denied.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
 
-#  rescue_from CanCan::AccessDenied do |exception|
-#    flash[:error] = exception.message
-#    redirect_to root_url
-#  end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_path, :alert => exception.message + " Please login to continue."
+  end
 end
