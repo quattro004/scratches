1faacd510d190762da922fbb6fb28a41cbc6b79e
quattro004 <quattro004@gmail.com>
2013-01-20 09:51:22 -0800

    Removed the nested_form gem since it wouldn't provide me with the behavior I wanted with respect to editing images on a recipe. Finished up recipe editing, ready for first release with the new layout :)

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'rmagick'
 gem "devise", ">= 2.1.0"
 gem 'cancan'
 gem 'bootstrap-sass'
-gem 'nested_form'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,17 @@
 //= require jquery_ujs
 //= require_self
 //= require bootstrap
-//= require jquery_nested_form
 //= require_tree .
 
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
+}
+
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
+  $(link).parent().before(content.replace(regexp, new_id));
+}
